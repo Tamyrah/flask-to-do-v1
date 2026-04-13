a8fd36feaf6cfd6d272c88bdde75bd319ca2ad3e
Myra M <myahmctear@gmail.com>
2026-04-13 09:47:52 -0400

    Check for todo.db and initialize database
    Import init_db if todo.db does not exist.

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 import os
 
+if not os.path.exists('todo.db'):
+    import init_db
+
+if not os.path.exists('todo.db'):
+    import init_db
+
 app = Flask(__name__)
 
 # Database file path
